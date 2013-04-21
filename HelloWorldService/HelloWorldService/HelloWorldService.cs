1b6c54f1797f9163d585e8c9b52febf3e6ba7dd1
lukasz-lysik <llysik@gmail.com>
2013-04-21 21:16:53 +0100

    HelloWorldService

---
+++ HelloWorldService/HelloWorldService/HelloWorldService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace HelloWorldService
+{
+    public class HelloWorldService : IHelloWorldService
+    {
+        public string SayHello(Person person)
+        {
+            return string.Format("Hello, {0} {1}", person.Name, person.Surname);
+        }
+    }
+}
---
+++ HelloWorldService/HelloWorldService/IHelloWorldService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace HelloWorldService
+{
+    [ServiceContract]
+    public interface IHelloWorldService
+    {
+        [OperationContract]
+        string SayHello(Person person);
+    }
+
+    [DataContract]
+    public class Person
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Surname { get; set; }
+    }
+}
