c0f9a44a23af136189871b0cedd4c486cf406c6e
lukasz-lysik <llysik@gmail.com>
2013-04-30 01:01:13 +0100

    Custom Service Host added

---
+++ HostingWebServices/CustomServiceHost/CustomServiceHost.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace CustomServiceHost
+{
+    public class CustomServiceHost : ServiceHost
+    {
+      public CustomServiceHost() : base(typeof(HelloWorldServiceLibrary.HelloWorldService))
+      {
+      }
+
+      protected override void OnOpening()
+      {
+        base.OnOpening();
+
+        var meta = this.Description.Behaviors.Find<ServiceMetadataBehavior>();
+        if (meta != null)
+        {
+          meta.HttpGetEnabled = true;
+        }
+        else
+        {
+          meta = new ServiceMetadataBehavior();
+          meta.HttpGetEnabled = true;
+          this.Description.Behaviors.Add(meta);
+        }
+
+      }
+    }
+}
